1834d4504f2195a6801a2935338c81bb56c83ec9
IBrazyI <73790113+IBrazyI@users.noreply.github.com>
2022-01-17 11:35:54 +0000

    Inital commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const http= require('http')
+const port = 3000
+
+const server = http.createServer(function(req, ses) {
+
+})
+
+server.listen(port, function(error){
+    if (error) {
+        console.log('Somthing went wrong', error)
+    } else {
+        console.log('Server is listening on port' + port)
+    }
+})
